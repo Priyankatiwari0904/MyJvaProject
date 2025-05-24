c837faa943cdb443484b81032dd0b83efb22557c
Priyanka tiwari <115725895+Priyankatiwari0904@users.noreply.github.com>
2025-05-24 17:12:00 +0530

    Add files via upload
    Add student and main java classes

---
+++ Main.java
@@ -0,0 +1,24 @@
+
+    
+
+class student{
+	String name;
+	int age;
+	int rollno;
+	void print() {
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(rollno);
+	}
+}
+public class Main {
+
+	public static void main(String[] args) {
+		
+       // student student1 = new student();
+       // student1.name="Radha";
+      //  student1.age=25;
+      //  student1.rollno=1;
+	}
+
+}
